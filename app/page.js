6671e6e6baee86423bc102f1b90afbe56e4adb4e
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-23 01:51:00 +0600

    Initial Commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <h1 className="text-5xl text-center p-8 bg-indigo-950 text-white">BD Consultancy Work in Progress</h1>
+    </div>
+  );
+}
