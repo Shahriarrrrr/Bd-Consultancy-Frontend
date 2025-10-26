6e84a1af20c6238d669cbf96498d8dd63149099d
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-26 18:53:22 +0600

    layout Start Nav

---
+++ app/(root)/layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+// app/(root)/layout.jsx
+export default function RootLayout({ children }) {
+  return (
+    <main className="font-work-sans">
+        <Navbar />
+
+        {children}
+    </main>
+  );
+}
